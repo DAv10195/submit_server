b67437c4cefbb4c5146575fb9d132bb46799dfd1
david.abramov <david.abramov@servicenow.com>
2021-05-06 19:01:29 +0300

    update routes

--- server/agents_endpoint.go
+++ server/agents_endpoint.go
@@ -217,7 +217,7 @@ func (m *agentEndpointsManager) agentStatusMonitor(ctx context.Context, wg *sync
 func initAgentsBackend(r *mux.Router, manager *authManager, ctx context.Context, wg *sync.WaitGroup) {
 	agentsBasePath := fmt.Sprintf("/%s", submitws.Agents)
 	agentsRouter := r.PathPrefix(agentsBasePath).Subrouter()
-	agentsRouter.HandleFunc(fmt.Sprintf("/%s", endpoint), agentEndpoints.agentsEndpoint)
+	agentsRouter.HandleFunc(fmt.Sprintf("/%s", endpoint), agentEndpoints.agentsEndpoint).Methods(http.MethodGet)
 	manager.addPathToMap(fmt.Sprintf("%s/%s", agentsBasePath, endpoint), func (user *users.User, _ string) bool {
 		return user.Roles.Contains(users.Agent) || user.Roles.Contains(users.Admin)
 	})
@@ -225,7 +225,7 @@ func initAgentsBackend(r *mux.Router, manager *authManager, ctx context.Context,
 	manager.addPathToMap(fmt.Sprintf("%s/", agentsBasePath), func (user *users.User, _ string) bool {
 		return user.Roles.Contains(users.Admin)
 	})
-	agentsRouter.HandleFunc(fmt.Sprintf("/{%s}", agentId), handleGetAgent).Methods(http.MethodGet)
+	agentsRouter.HandleFunc(fmt.Sprintf("/{%s}/get", agentId), handleGetAgent).Methods(http.MethodGet)
 	manager.addRegex(regexp.MustCompile(fmt.Sprintf("%s/.", agentsBasePath)), func (user *users.User, _ string) bool {
 		return user.Roles.Contains(users.Admin)
 	})
